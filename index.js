466b374f1b190084d7148cd1a30186e9c8a76fa7
Anass Bouabdellaoui <anassbouabdellaoui1@gmail.com>
2026-01-31 16:33:44 +0100

    Fixing bugs

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+container.classList.add('small');
+container.classList.remove('large');
+
+function restartFadeIn(el) {
+  el.classList.remove('fadeIn');
+  void el.offsetHeight;
+  el.classList.add('fadeIn');
+}
+
+
 
 search.addEventListener('click', () => {
-    const APIKey = '60d07ad6727e025ac08ca70ff051d1de';
+    const APIKey = 'Your api key here';
     const city = document.querySelector('.search-box input').value;
-    if (city === '') return;
+    if (city === '')
+        return;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
         if (json.cod === '404') {
-            container.style.height = '400px';
+            container.classList.add('large');
+            container.classList.remove('small');
             weatherBox.style.display = 'none';
             weatherDetails.style.display = 'none';
             error404.style.display = 'block';
-            error404.classList.add('fadeIn');
+            weatherBox.classList.remove('fadeIn');
+            weatherDetails.classList.remove('fadeIn');
+            restartFadeIn(error404);
             return;
         }
         error404.style.display = 'none';
@@ -24,7 +38,7 @@ search.addEventListener('click', () => {
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity span');
         const wind = document.querySelector('.weather-details .wind span');
-        
+       
         switch (json.weather[0].main) {
             case 'Clear':
                 image.src = 'images/clear.png';
@@ -38,7 +52,7 @@ search.addEventListener('click', () => {
             case 'Clouds':
                 image.src = 'images/cloud.png';
                 break;
-            case 'Haze':
+            case 'Mist':
                 image.src = 'images/mist.png';
                 break;
             default:
@@ -50,8 +64,9 @@ search.addEventListener('click', () => {
         wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
         weatherBox.style.display = '';
         weatherDetails.style.display = '';
-        weatherBox.classList.add('fadeIn');
-        weatherDetails.classList.add('fadeIn');
-        container.style.height = '590px';
-    })
-})
+        container.classList.add('large');
+        container.classList.remove('small');
+        restartFadeIn(weatherBox);
+        restartFadeIn(weatherDetails);
+    });
+});
